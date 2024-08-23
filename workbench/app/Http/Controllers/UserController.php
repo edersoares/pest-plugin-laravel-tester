5b2f31e101c8dbe6ac7cb44458f131e9df110643
Eder Soares <edersoares@me.com>
2024-08-23 19:30:50 -0300

    Allows to wrap endpoint result

--- workbench/app/Http/Controllers/UserController.php
+++ workbench/app/Http/Controllers/UserController.php
@@ -8,6 +8,22 @@ use Workbench\App\Models\User;
 
 class UserController
 {
+    public function index()
+    {
+        if (request('nowrap')) {
+            return User::query()->get();
+        } else {
+            return User::query()->paginate();
+        }
+    }
+
+    public function show(string $id)
+    {
+        return [
+            'user' => User::query()->findOrFail($id),
+        ];
+    }
+
     public function destroy(string $id)
     {
         $model = User::query()->findOrFail($id);
