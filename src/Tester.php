21b41f894d26ce7fa9de778a14103c98b5ba76da
Eder Soares <edersoares@me.com>
2024-08-23 15:38:27 -0300

    Tester

---
+++ src/Tester.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dex\Pest\Plugin\Laravel\Tester;
+
+use Illuminate\Foundation\Testing\TestCase;
+
+/**
+ * @mixin TestCase
+ */
+trait Tester
+{
+    //
+}
