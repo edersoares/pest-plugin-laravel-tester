3316914b19a6722cef4920b07fc2c973d0e32514
Eder Soares <edersoares@me.com>
2024-08-23 16:40:29 -0300

    Code style

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -20,11 +20,11 @@ class WorkbenchServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+            $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         }
 
-        if (file_exists(__DIR__ . '/../../routes/api.php')) {
-            $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+        if (file_exists(__DIR__.'/../../routes/api.php')) {
+            $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
         }
     }
 }
