41471e10fa352787d9365b5d0a8cb7a1501a1ec0
Eder Soares <edersoares@me.com>
2024-08-23 17:18:24 -0300

    Ensures `toBeDelete` works when using soft deletes

--- workbench/database/migrations/0000_00_00_000000_create_user_table.php
+++ workbench/database/migrations/0000_00_00_000000_create_user_table.php
@@ -14,6 +14,7 @@ class CreateUserTable extends Migration
             $table->id();
             $table->string('name');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
